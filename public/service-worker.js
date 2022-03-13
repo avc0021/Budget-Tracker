0e773aa720a19a200784e7c49bf7acad3388ae4a
avc0021 <avc0021@gmail.com>
2022-03-12 19:39:22 -0600

    issue resolve for service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,10 @@ const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-    "..public/index.html",
+    "./index.html",
     "./css/style.css",
-    "./js/idb",
-    "./js/index"
+    "./js/idb.js",
+    "js/index.js"
   ];
 
 // Respond with cached resources
